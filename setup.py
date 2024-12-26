77c207d7c07637fdc749ed9d1e4daa96a0a0291b
Jeremy Lane <jeremylanes17@gmail.com>
2024-12-26 00:46:49 -1000

    manual mention of bin grazing

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="latex-to-text",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(include=["bin", "bin.*"]),
     description="Convert LaTeX to text",
     author="arnbod",
     url="https://github.com/jeremylanes/latex-to-text.git",
     license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
 )
