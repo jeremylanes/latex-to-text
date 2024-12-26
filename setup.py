da5e4d18a7cb154c93c90b142c042c574f3a40d4
Jeremy Lane <jeremylanes17@gmail.com>
2024-12-26 00:27:39 -1000

    Added setup.py and restructured as Python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="latex-to-text",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Convert LaTeX to text",
+    author="arnbod",
+    url="https://github.com/jeremylanes/latex-to-text.git",
+    license="GPLv3",
+)
