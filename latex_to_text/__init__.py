8b31edd08788de6ed92b052e61f68ae159c1cde1
Jeremy Lane <jeremylanes17@gmail.com>
2024-12-26 07:08:04 -1000

    performed convert_text_to_latex

--- latex_to_text/__init__.py
+++ latex_to_text/__init__.py
@@ -1,1 +1,2 @@
 from latextotext import convert_latex_to_text
+from texttolatex import convert_text_to_latex
--- latex_to_text/texttolatex.py
+++ latex_to_text/texttolatex.py
@@ -8,7 +8,6 @@
 
 import argparse
 import re
-import sys
 import os
 import yaml
 from constants import *
@@ -17,68 +16,70 @@ from constants_perso import *  # Personal customization
 #--------------------------------------------------
 #--------------------------------------------------
 
-# Arguments 
-parser = argparse.ArgumentParser(description='Conversion a text file to a LaTeX gluing back commands and maths.')
-parser.add_argument('inputfile', help='input text filename')
-parser.add_argument('dicfile', nargs='?', help='input dictionary filename')
-parser.add_argument('outputfile', nargs='?', help='output LaTeX filename')
-options = parser.parse_args()
+def convert_text_to_latex(txt_file, dictionary_file=None, output_file=None):
+    """
+    Converts a text file and its dictionary file to a LaTeX file.
 
-txt_file = options.inputfile
-output_file = options.outputfile
-dictionary_file = options.dicfile
+    :param txt_file: Path to the input text file.
+    :param dictionary_file: Path to the input dictionary file (optional).
+    :param output_file: Path to the output LaTeX file (optional).
+    :return: None
+    """
+    # Get argument: a txt file
+    file_name, _ = os.path.splitext(txt_file)
 
+    # Dictionary file name
+    if dictionary_file:
+        dic_file = dictionary_file    # Name given by user
+    else:
+        dic_file = file_name + '.dic'  # If no name add a .dic extension
 
-# Get argument: a txt file
-file_name, file_extension = os.path.splitext(txt_file) 
+    # Output file name
+    if output_file:
+        tex_file = output_file    # Name given by user
+    else:
+        tex_file = file_name + '.tex'  # If no name add a .tex extension
 
+    # Read file object to string
+    with open(txt_file, 'r', encoding='utf-8') as fic_txt:
+        text_all = fic_txt.read()
 
-# Dictionary file name 
-if dictionary_file:
-    dic_file = dictionary_file    # Name given by user
-else:
-    dic_file = file_name+'.dic' # If no name add a .dic extension
+    # Read dictionary
+    with open(dic_file, 'r', encoding='utf-8') as fic_dic:
+        dictionary = yaml.load(fic_dic, Loader=yaml.BaseLoader)
 
+    # Replacements start now
+    text_new = text_all
 
-# Output file name 
-if output_file:
-    tex_file = output_file    # Name given by user
-else:
-    tex_file = file_name+'.tex' # If no name add a .tex extension
+    # Iterate replacing until nothing remains to be replaced
+    # (to deal with nested replacements)
+    keep_replacing = True
+    k = 0
+    while keep_replacing:
+        keep_replacing = False
+        for i, val in dictionary.items():
+            tag_str = tag + str(i) + tag
+            val = val.replace('\\', '\\\\')  # double \\ for correct write
+            (text_new, number_of_subs_made) = re.subn(tag_str, val, text_new, flags=re.MULTILINE | re.DOTALL)
+            if number_of_subs_made > 0:
+                keep_replacing = True
+        k += 1
+    # print(f'{k} iteration(s) done.')
 
-# Read file object to string
-fic_txt = open(txt_file, 'r', encoding='utf-8')
-text_all = fic_txt.read()
-fic_txt.close()
+    # Write the result
+    with open(tex_file, 'w', encoding='utf-8') as fic_tex:
+        fic_tex.write(text_new)
 
+if __name__ == "__main__":
+    # Arguments
+    parser = argparse.ArgumentParser(description='Conversion a text file to a LaTeX gluing back commands and maths.')
+    parser.add_argument('inputfile', help='input text filename')
+    parser.add_argument('dicfile', nargs='?', help='input dictionary filename')
+    parser.add_argument('outputfile', nargs='?', help='output LaTeX filename')
+    options = parser.parse_args()
 
-# Read dictionary
-fic_dic = open(dic_file, 'r', encoding='utf-8')
-dictionary = yaml.load(fic_dic, Loader=yaml.BaseLoader)
-fic_dic.close()
-
-
-# Replacements start now
-text_new = text_all
-
-# Iterate replacing until nothing remains to be replaced
-# (to deal with nested replacements)
-keep_replacing = True
-k = 0
-while keep_replacing:
-    keep_replacing = False
-    for i,val in dictionary.items():
-        tag_str = tag+str(i)+tag
-        val = val.replace('\\','\\\\')    # double \\ for correct write
-        # val = re.escape(val)
-        (text_new, number_of_subs_made) = re.subn(tag_str,val,text_new, flags=re.MULTILINE|re.DOTALL)
-        if number_of_subs_made > 0:
-            keep_replacing = True
-    k += 1
-# print(f'{k} iteration(s) done.')
-
-# Write the result
-with open(tex_file, 'w', encoding='utf-8') as fic_tex:
-    fic_tex.write(text_new)
-
-
+    convert_text_to_latex(
+        txt_file=options.inputfile,
+        dictionary_file=options.dicfile,
+        output_file=options.outputfile
+    )
