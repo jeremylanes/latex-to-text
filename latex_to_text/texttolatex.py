aa58fd04fec735fcdc67276ccebd487b7718bfa8
Jeremy Lane <jeremylanes17@gmail.com>
2025-04-10 01:04:05 -1000

    refactor text to latex

--- latex_to_text/texttolatex.py
+++ latex_to_text/texttolatex.py
@@ -9,71 +9,78 @@
 import argparse
 import re
 import os
+from dataclasses import dataclass
+
 import yaml
-from .constants import *
+from constants import *
 # from .constants_perso import *  # Personal customization
 
 #--------------------------------------------------
 #--------------------------------------------------
 
-def convert_text_to_latex(txt_file, dictionary_file=None, output_file=None):
-    """
-    Converts a text file and its dictionary file to a LaTeX file.
+@dataclass
+class TextToLatex:
+    open_tag: str = tag
+    close_tag: str = close_tag
 
-    :param txt_file: Path to the input text file.
-    :param dictionary_file: Path to the input dictionary file (optional).
-    :param output_file: Path to the output LaTeX file (optional).
-    :return: None
-    """
-    # Get argument: a txt file
-    file_name, _ = os.path.splitext(txt_file)
+    def convert_text_to_latex(self, txt_file, dictionary_file=None, output_file=None):
+        """
+        Converts a text file and its dictionary file to a LaTeX file.
 
-    # Dictionary file name
-    if dictionary_file:
-        dic_file = dictionary_file    # Name given by user
-    else:
-        dic_file = file_name + '.dic'  # If no name add a .dic extension
+        :param txt_file: Path to the input text file.
+        :param dictionary_file: Path to the input dictionary file (optional).
+        :param output_file: Path to the output LaTeX file (optional).
+        :return: None
+        """
+        # Get argument: a txt file
+        file_name, _ = os.path.splitext(txt_file)
 
-    # Output file name
-    if output_file:
-        tex_file = output_file    # Name given by user
-    else:
-        tex_file = file_name + '.tex'  # If no name add a .tex extension
+        # Dictionary file name
+        if dictionary_file:
+            dic_file = dictionary_file    # Name given by user
+        else:
+            dic_file = file_name + '.dic'  # If no name add a .dic extension
 
-    # Read file object to string
-    with open(txt_file, 'r', encoding='utf-8') as fic_txt:
-        text_all = fic_txt.read()
+        # Output file name
+        if output_file:
+            tex_file = output_file    # Name given by user
+        else:
+            tex_file = file_name + '.tex'  # If no name add a .tex extension
 
-    # Read dictionary
-    with open(dic_file, 'r', encoding='utf-8') as fic_dic:
-        dictionary = yaml.load(fic_dic, Loader=yaml.BaseLoader)
+        # Read file object to string
+        with open(txt_file, 'r', encoding='utf-8') as fic_txt:
+            text_all = fic_txt.read()
 
-    # Replacements start now
-    text_new = text_all
+        # Read dictionary
+        with open(dic_file, 'r', encoding='utf-8') as fic_dic:
+            dictionary = yaml.load(fic_dic, Loader=yaml.BaseLoader)
 
-    # Iterate replacing until nothing remains to be replaced
-    # (to deal with nested replacements)
-    keep_replacing = True
-    k = 0
-    while keep_replacing:
-        keep_replacing = False
-        for i, val in dictionary.items():
-            tag_str = tag + str(i) + close_tag
-            val = val.replace('\\', '\\\\')  # double \\ for correct write
-            (text_new, number_of_subs_made) = re.subn(tag_str, val, text_new, flags=re.MULTILINE | re.DOTALL)
-            if number_of_subs_made > 0:
-                keep_replacing = True
-        k += 1
-    # print(f'{k} iteration(s) done.')
+        # Replacements start now
+        text_new = text_all
 
-        # Write output files
-        abs_new_txt_file = os.path.abspath(text_new)
+        # Iterate replacing until nothing remains to be replaced
+        # (to deal with nested replacements)
+        keep_replacing = True
+        k = 0
+        while keep_replacing:
+            keep_replacing = False
+            for i, val in dictionary.items():
+                tag_str = self.open_tag + str(i) + self.close_tag
+                val = val.replace('\\', '\\\\')  # double \\ for correct write
+                (text_new, number_of_subs_made) = re.subn(tag_str, val, text_new, flags=re.MULTILINE | re.DOTALL)
+                if number_of_subs_made > 0:
+                    keep_replacing = True
+            k += 1
+        # print(f'{k} iteration(s) done.')
 
-    # Write the result
-    with open(tex_file, 'w', encoding='utf-8') as fic_tex:
-        fic_tex.write(text_new)
+            # Write output files
+            abs_new_txt_file = os.path.abspath(text_new)
 
-    return abs_new_txt_file
+        # Write the result
+        with open(tex_file, 'w', encoding='utf-8') as fic_tex:
+            fic_tex.write(text_new)
+
+        return abs_new_txt_file
 
 if __name__ == "__main__":
     # Arguments
@@ -83,7 +90,7 @@ if __name__ == "__main__":
     parser.add_argument('outputfile', nargs='?', help='output LaTeX filename')
     options = parser.parse_args()
 
-    convert_text_to_latex(
+    TextToLatex().convert_text_to_latex(
         txt_file=options.inputfile,
         dictionary_file=options.dicfile,
         output_file=options.outputfile
